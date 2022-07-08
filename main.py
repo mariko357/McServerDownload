8cdc62861785eb3ec20c350c8d3c32c304ee0554
marko <belenkov.marko@gmail.com>
2022-07-08 17:29:05 +0300

    Added 40 handler

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ def getDownloadHref(href):
     href = res.get("href")
     return href
 
+def validateURL(href):
+    page = requests.get(href)
+    if str(page) != "<Response [404]>":
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download Server Jar File")
 
@@ -19,26 +26,37 @@ if __name__ == "__main__":
     parser.add_argument("--download", "-d", help="Download Specific Server Version", type = str)
     parser.add_argument("--link", "-l", help="Download Server From Specific Link", type = str)
     parser.add_argument("--path", "-p", help="Select download path", type = str)
-    parser.add_argument("--bar", "-b", help="Display download bar (true/false), Default = false", nargs = "?", const = "false", type = str)
+    parser.add_argument("--bar", "-b", help="Display download bar", nargs = "?", const = "true", default = "false", type = str)
 
     options, args = parser.parse_known_args()
+
+    if options.bar != "false":
+        bar = wget.bar_adaptive
+    else:
+        bar = None
 
 
     if options.link:
         if options.path:
-            wget.download(options.link, options.path)
+            wget.download(options.link, options.path, bar = bar)
         else:
-            wget.download(options.link)
+            wget.download(options.link, bar = bar)
 
     elif options.download:
 
         href = BASE_HREF + options.download
-        downHref = getDownloadHref(href)
 
-        if options.path:
-            wget.download(downHref, options.path)
+        if validateURL(href):
+
+            downHref = getDownloadHref(href)
+
+            if options.path:
+                wget.download(downHref, options.path, bar = bar)
+            else:
+                wget.download(downHref, bar = bar)
         else:
-            wget.download(downHref)
+            print("No such verison")
+            exit()
         
     else:
         print("No arguments provided! Exiting without action!")
