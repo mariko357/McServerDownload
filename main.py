34627d35789b25ed2e1d48362f6001e3b95bd69b
marko <belenkov.marko@gmail.com>
2022-07-17 23:12:35 +0300

    Fixed issue with download path selection

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from ServerDownloader.DownSrv import ServerDownloader
-import wget
 import time
 
 if __name__ == "__main__":
-    inst = ServerDownloader()
+    inst = ServerDownloader("cache/")
     j = 0
-    for i in inst.snapshotVersion:
-        print(i, j)
+    for i in inst.stableVersion:
+        inst.downloadServer(i)
+        print(i)
         j += 1
-    print(inst.stableVersion[5])
-    time.sleep(10)
-    inst.downloadServer(inst.stableVersion[5])
-    j = 0
